116b205c41aa72252073a34df76d152903c6223e
chezmarcbrown <marc@chezbrown.org>
2022-10-11 14:43:12 -0800

    stuff

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Airport, Flight
+
+admin.site.register(Airport)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("duration", "__str__")
+
+admin.site.register(Flight, FlightAdmin)

3d6c8ef66f5d61fbdb306454852aee35e6e507b7
chezmarcbrown <marc@chezbrown.org>
2022-10-11 19:05:18 -0800

    end of class -

--- flights/admin.py
+++ flights/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Airport, Flight
+from .models import Airport, Flight, Passenger
 
 admin.site.register(Airport)
 
@@ -9,3 +9,4 @@ class FlightAdmin(admin.ModelAdmin):
     list_display = ("duration", "__str__")
 
 admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger)
--- flights/views.py
+++ flights/views.py
@@ -7,3 +7,15 @@ def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
     })
+
+from django.http import Http404
+
+def flight(request, flight_id):
+    try:
+        f = Flight.objects.get(id = flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("flight not found")
+    return render(request, "flights/flight.html", {
+        "flight": f,
+        "passengers": f.passengers.all()
+    })
